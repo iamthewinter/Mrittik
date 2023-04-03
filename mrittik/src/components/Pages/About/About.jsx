d902a5a46804083cba6e7236c65111cf809eb92c
Azer Mammadov <mammadovazer98@gmail.com>
2023-04-03 22:06:15 +0400

    Animated background lines were added

--- mrittik/src/components/Pages/About/About.jsx
+++ mrittik/src/components/Pages/About/About.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import OurMission from "./OurMission/OurMission";
-
+import "../About/About.scss";
 const About = () => {
   return (
-    <>
+    <main className="about_main">
       <OurMission />
-    </>
+      <ul className="net_lines justify-content-between">
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+      </ul>
+    </main>
   );
 };
 
