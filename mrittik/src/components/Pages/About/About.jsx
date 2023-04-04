9ad0973f31a2f589105f5526a13c2fff2b8ec79f
Azer Mammadov <mammadovazer98@gmail.com>
2023-04-04 18:14:52 +0400

    OurMission section with module scss was added to About page

--- mrittik/src/components/Pages/About/About.jsx
+++ mrittik/src/components/Pages/About/About.jsx
@@ -3,8 +3,9 @@ import OurMission from "./OurMission/OurMission";
 import "../About/About.scss";
 const About = () => {
   return (
-    <main className="about_main">
+    <div className="about_main">
       <OurMission />
+
       <ul className="net_lines justify-content-between">
         <li className="animated_line"></li>
         <li className="animated_line"></li>
@@ -14,7 +15,7 @@ const About = () => {
         <li className="animated_line"></li>
         <li className="animated_line"></li>
       </ul>
-    </main>
+    </div>
   );
 };
 
--- mrittik/src/components/Pages/About/OurMission/OurMission.jsx
+++ mrittik/src/components/Pages/About/OurMission/OurMission.jsx
@@ -2,15 +2,54 @@ import React from "react";
 import OurMissionStyle from "../OurMission/OurMission.module.scss";
 const OurMission = () => {
   return (
-    <section className={OurMissionStyle.first_section}>
-      <div className="container">
+    <section className={`mission_section ${OurMissionStyle.mission_section}`}>
+      <div className={`container ${OurMissionStyle.px_5}`}>
         <div className="row justify-content-center">
-          <div className="col-xl-11">
-            <div className={OurMissionStyle.page_heading}>
+          <div className="col-xl-12">
+            <div className={`page_heading ${OurMissionStyle.page_heading}`}>
               <h6>OUR MISSION</h6>
               <p>
                 Our organizational sprit believes that where design meets
                 passion & love with space, good architecture happens here
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className={`row ${OurMissionStyle.px_5}`}>
+          <div className="col-xl-12 px-0">
+            <div className={OurMissionStyle.hidden_line}>
+              <img
+                src="https://wpthemebooster.com/demo/themeforest/html/mrittik/assets/img/bg/about_bg.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className={`row justify-content-center ${OurMissionStyle.px_5}`}>
+          <div className="col-xl-4 col-lg-4 pe-lg-4">
+            <div className={`col4_adapter ${OurMissionStyle.col4_adapter}`}>
+              <h3>ARCHITECTURE IS WHERE WE BELONG</h3>
+            </div>
+          </div>
+          <div className="col-xl-6 col-lg-6 ps-lg-4">
+            <div className={OurMissionStyle.col6_adapter}>
+              <p>
+                Mrittik Architects is a full-service design firm providing
+                architecture, master planning, urban design, interior
+                architecture, space planning and programming. Our portfolio of
+                completed work includes highly acclaimed and award-winning
+                projects for clients.
+              </p>
+              <p>
+                Mrittik Architects is a full-service design firm providing
+                architecture, master planning, urban design, interior
+                architecture, space planning and programming. Our portfolio of
+                completed work includes highly acclaimed and award-winning
+                projects for clients.
+              </p>
+              <p>
+                Mrittik Architects is a full-service design firm providing
+                architecture, master planning, urban design.
               </p>
             </div>
           </div>
