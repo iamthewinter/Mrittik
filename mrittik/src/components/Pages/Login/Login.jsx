cc6ce363968986cd380ea6c25821c095fd960eba
Azer Mammadov <mammadovazer98@gmail.com>
2023-04-07 21:02:13 +0400

    Login page was created

--- mrittik/src/components/Pages/Login/Login.jsx
+++ mrittik/src/components/Pages/Login/Login.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import LoginHeading from "./LoginHeading/LoginHeading";
+import "../Login/Login.scss";
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login_main">
+      <LoginHeading />
+      <ul className="net_lines justify-content-between">
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+      </ul>
+    </div>
+  );
 };
 
 export default Login;
---
+++ mrittik/src/components/Pages/Login/LoginHeading/LoginHeading.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../LoginHeading/LoginHeading.scss";
+const LoginHeading = () => {
+  return (
+    <section className="loginpage_heading">
+      <div
+        className="head_back_image"
+        style={{
+          backgroundImage: `url("https://wpthemebooster.com/demo/themeforest/html/mrittik/assets/img/header_bg.jpg")`,
+        }}
+      >
+        <div className="container px_5">
+          <div className="row justify-content-between align-items-center px_5">
+            <div className="col-xl-2 col-lg-2 col-md-3 col-sm-4 px-0">
+              <div className="left_head">
+                <h2>Login</h2>
+              </div>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-7 col-sm-8 px-0">
+              <ul className="page_location">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="#">Pages</Link>
+                </li>
+                <li>
+                  <Link to="#">Shop</Link>
+                </li>
+                <li>Login</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LoginHeading;
