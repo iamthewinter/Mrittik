fb9b5daf5e49686b12e8410984b377fe8e3ee49b
Azer Mammadov <mammadovazer98@gmail.com>
2023-04-07 17:41:32 +0400

    Google map added

--- mrittik/src/components/Pages/Contacts/Contacts.jsx
+++ mrittik/src/components/Pages/Contacts/Contacts.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ContactHeading from "./ContactHeading/ContactHeading";
-
+import "./Contacts.scss";
+import GMap from "./GMap/GMap";
 const Contacts = () => {
   return (
-    <>
+    <div className="contacts_main">
       <ContactHeading />
-    </>
+      <GMap />
+      <ul className="net_lines justify-content-between">
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+      </ul>
+    </div>
   );
 };
 
