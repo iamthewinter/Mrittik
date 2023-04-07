7af6357a9ab78ede8f90f61df4489a3fcd358bd6
Azer Mammadov <mammadovazer98@gmail.com>
2023-04-07 22:47:50 +0400

    Login page background images were added

--- mrittik/src/components/Pages/Login/Login.jsx
+++ mrittik/src/components/Pages/Login/Login.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import LoginHeading from "./LoginHeading/LoginHeading";
 import "../Login/Login.scss";
+import LoginForm from "./LoginForm/LoginForm";
 const Login = () => {
   return (
     <div className="login_main">
       <LoginHeading />
+      <LoginForm />
+      <div className="loginpage_backimage"></div>
       <ul className="net_lines justify-content-between">
         <li className="animated_line"></li>
         <li className="animated_line"></li>
