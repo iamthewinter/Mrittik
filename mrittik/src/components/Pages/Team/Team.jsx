852491b8648ca029c9861343c84f6a60cdc615d1
Azer Mammadov <mammadovazer98@gmail.com>
2023-04-08 23:09:23 +0400

    Team page was created

--- mrittik/src/components/Pages/Team/Team.jsx
+++ mrittik/src/components/Pages/Team/Team.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import TeamHeading from "./TeamHeading/TeamHeading";
 
 const Team = () => {
-  return <div>Team</div>;
+  return (
+    <div className="team_main">
+      <TeamHeading />
+      <ul className="net_lines justify-content-between">
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+      </ul>
+    </div>
+  );
 };
 
 export default Team;
---
+++ mrittik/src/components/Pages/Team/TeamHeading/TeamHeading.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "../TeamHeading/TeamHeading.scss";
+import { Link } from "react-router-dom";
+
+const TeamHeading = () => {
+  return (
+    <section className="teamtpage_heading">
+      <div
+        className="head_back_image"
+        style={{
+          backgroundImage: `url("https://wpthemebooster.com/demo/themeforest/html/mrittik/assets/img/header_bg.jpg")`,
+        }}
+      >
+        <div className="container px_5">
+          <div className="row justify-content-between align-items-center px_5">
+            <div className="col-xl-2 col-lg-2 col-md-4 col-sm-6 col_xs_6 px-0">
+              <div className="left_head">
+                <h2>Contact</h2>
+              </div>
+            </div>
+            <div className="col-xl-2 col-lg-2 col-md-4 col-sm-6 col_xs_6 px-0">
+              <ul className="page_location">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>Contact</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TeamHeading;
