c1426ada9200a9ca9f416cdb7a511a6137b278d4
Azer Mammadov <mammadovazer98@gmail.com>
2023-04-14 22:51:29 +0400

    Projects page was created

--- mrittik/src/components/Pages/Projects/Projects.jsx
+++ mrittik/src/components/Pages/Projects/Projects.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import "../Projects/Projects.scss";
+import ProjectsHeading from "./ProjectsHeading/ProjectsHeading";
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="projects_main">
+      <ProjectsHeading />
+      <ul className="net_lines justify-content-between">
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+      </ul>
+    </div>
+  );
 };
 
 export default Projects;
