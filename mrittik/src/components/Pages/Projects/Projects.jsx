f654a704ac6c499a4e0b68c7da98ef238b25c8af
Azer Mammadov <mammadovazer98@gmail.com>
2023-04-15 15:56:10 +0400

    ProjectsFilter section was added

--- mrittik/src/components/Pages/Projects/Projects.jsx
+++ mrittik/src/components/Pages/Projects/Projects.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "../Projects/Projects.scss";
 import ProjectsHeading from "./ProjectsHeading/ProjectsHeading";
+import ProjectsFilter from "./ProjectsFilter/ProjectsFilter";
 const Projects = () => {
   return (
     <div className="projects_main">
       <ProjectsHeading />
+      <ProjectsFilter />
       <ul className="net_lines justify-content-between">
         <li className="animated_line"></li>
         <li className="animated_line"></li>
