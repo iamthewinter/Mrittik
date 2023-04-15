2e82943b3a172c51b62065e0680cc65c531d3ecb
Azer Mammadov <mammadovazer98@gmail.com>
2023-04-15 20:12:16 +0400

    Service page was created

--- mrittik/src/components/Pages/Service/Service.jsx
+++ mrittik/src/components/Pages/Service/Service.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import "../Service/Service.scss";
+import ServiceHeading from "./ServiceHeading/ServiceHeading";
 const Service = () => {
-  return <div>Service</div>;
+  return (
+    <div className="service_main">
+      <ServiceHeading />
+      <div className="servicepage_backimage"></div>
+      <ul className="net_lines justify-content-between">
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+      </ul>
+    </div>
+  );
 };
 
 export default Service;
