175e97c17229aaca1ea22ba45ae928e179fadfdb
Azer Mammadov <mammadovazer98@gmail.com>
2023-04-17 18:05:51 +0400

    Ourpartners section and Service page was finished

---
+++ mrittik/src/components/Pages/Service/Ourpartners/Ourpartners.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import "../Ourpartners/Ourpartners.scss";
+const Ourpartners = () => {
+  return (
+    <section className="ourpartners_section">
+      <ul className="net_lines justify-content-between">
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+        <li className="animated_line"></li>
+      </ul>
+      <div className="container">
+        <div className="our_partners px_5">
+          <div className="partner_logo">
+            <img
+              src="https://wpthemebooster.com/demo/themeforest/html/mrittik/assets/img/partner/5.svg"
+              alt=""
+            />
+            <img
+              src="https://wpthemebooster.com/demo/themeforest/html/mrittik/assets/img/partner/55.svg"
+              alt=""
+            />
+          </div>
+          <div className="partner_logo">
+            <img
+              src="https://wpthemebooster.com/demo/themeforest/html/mrittik/assets/img/partner/2.svg"
+              alt=""
+            />
+            <img
+              src="https://wpthemebooster.com/demo/themeforest/html/mrittik/assets/img/partner/22.svg"
+              alt=""
+            />
+          </div>
+          <div className="partner_logo">
+            <img
+              src="https://wpthemebooster.com/demo/themeforest/html/mrittik/assets/img/partner/4.svg"
+              alt=""
+            />
+            <img
+              src="https://wpthemebooster.com/demo/themeforest/html/mrittik/assets/img/partner/44.svg"
+              alt=""
+            />
+          </div>
+          <div className="partner_logo">
+            <img
+              src="https://wpthemebooster.com/demo/themeforest/html/mrittik/assets/img/partner/1.svg"
+              alt=""
+            />
+            <img
+              src="https://wpthemebooster.com/demo/themeforest/html/mrittik/assets/img/partner/11.svg"
+              alt=""
+            />
+          </div>
+          <div className="partner_logo">
+            <img
+              src="https://wpthemebooster.com/demo/themeforest/html/mrittik/assets/img/partner/3.svg"
+              alt=""
+            />
+            <img
+              src="https://wpthemebooster.com/demo/themeforest/html/mrittik/assets/img/partner/33.svg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Ourpartners;
--- mrittik/src/components/Pages/Service/Service.jsx
+++ mrittik/src/components/Pages/Service/Service.jsx
@@ -3,12 +3,14 @@ import "../Service/Service.scss";
 import ServiceHeading from "./ServiceHeading/ServiceHeading";
 import ServiceCards from "./ServiceCards/ServiceCards";
 import BlackSwipeBox from "./BlackSwipeBox/BlackSwipeBox";
+import Ourpartners from "./Ourpartners/Ourpartners";
 const Service = () => {
   return (
     <div className="service_main">
       <ServiceHeading />
       <ServiceCards />
       <BlackSwipeBox />
+      <Ourpartners />
       <div className="servicepage_backimage"></div>
       <ul className="net_lines justify-content-between">
         <li className="animated_line"></li>
