0dcb70e98a4e7c7f430511bf186d14f7e08f080c
Azer Mammadov <mammadovazer98@gmail.com>
2023-04-24 02:33:53 +0400

    Preloader added to About page

--- mrittik/src/components/Pages/About/About.jsx
+++ mrittik/src/components/Pages/About/About.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import OurMission from "./OurMission/OurMission";
 import "../About/About.scss";
 import Video from "./Video/Video";
@@ -8,27 +8,47 @@ import Underlined from "./Underlined/Underlined";
 import TeamSwipe from "./TeamSwipe/TeamSwipe";
 import BlckBoxSwipe from "./BlckBoxSwipe/BlckBoxSwipe";
 import LetsDiscuss from "./LetsDiscuss/LetsDiscuss";
+import { motion } from "framer-motion";
+import Preloader from "../../Preloader/Preloader";
 const About = () => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2900);
+  }, []);
   return (
-    <div className="about_main">
-      <OurMission />
-      <Video />
-      <Facts />
-      <Service />
-      <Underlined />
-      <TeamSwipe />
-      <BlckBoxSwipe />
-      <LetsDiscuss />
-      <ul className="net_lines justify-content-between">
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-      </ul>
-    </div>
+    <>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <motion.div
+          className="about_main"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0, duration: 1 }}
+        >
+          <OurMission />
+          <Video />
+          <Facts />
+          <Service />
+          <Underlined />
+          <TeamSwipe />
+          <BlckBoxSwipe />
+          <LetsDiscuss />
+          <ul className="net_lines justify-content-between">
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+          </ul>
+        </motion.div>
+      )}
+    </>
   );
 };
 
