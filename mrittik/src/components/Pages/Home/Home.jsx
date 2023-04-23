5e8704192d2b6248987f726a38c497d17332bd6c
Azer Mammadov <mammadovazer98@gmail.com>
2023-04-23 23:05:59 +0400

    Preloader added to Home page and changes in footer

--- mrittik/src/components/Pages/Home/Home.jsx
+++ mrittik/src/components/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AboutUs from "./AboutUs/AboutUs";
 import Achievements from "./Achievements/Achievements";
 import FirstSwiper from "./FirstSwiper/FirstSwiper";
@@ -8,19 +8,39 @@ import GalleryIsotope from "./GalleryIsotope/GalleryIsotope";
 import BlackBoxSwipe from "./BlackBoxSwipe/BlackBoxSwipe";
 import LatestBlogs from "./LatestBlogs/LatestBlogs";
 import ContactUs from "./ContactUs/ContactUs";
+import { motion } from "framer-motion";
+import Preloader from "../../Preloader/Preloader";
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2900);
+  }, []);
   return (
     <>
-      <FirstSwiper />
-      <Services />
-      <AboutUs />
-      <Achievements />
-      <Running />
-      <GalleryIsotope />
-      <BlackBoxSwipe />
-      <LatestBlogs />
-      <ContactUs />
+      {loading ? (
+        <Preloader />
+      ) : (
+        <motion.div
+          className="home_main"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0, duration: 1 }}
+        >
+          <FirstSwiper />
+          <Services />
+          <AboutUs />
+          <Achievements />
+          <Running />
+          <GalleryIsotope />
+          <BlackBoxSwipe />
+          <LatestBlogs />
+          <ContactUs />
+        </motion.div>
+      )}
     </>
   );
 };
