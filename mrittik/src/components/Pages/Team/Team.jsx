1839e6ce803660f3568c7e214d9dc5e0ee3d147b
Azer Mammadov <mammadovazer98@gmail.com>
2023-04-24 22:39:36 +0400

    Preloader added to Team page

--- mrittik/src/components/Pages/Team/Team.jsx
+++ mrittik/src/components/Pages/Team/Team.jsx
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TeamHeading from "./TeamHeading/TeamHeading";
 import "../Team/Team.scss";
 import OurTeam from "./OurTeam/OurTeam";
+import { motion } from "framer-motion";
+import Preloader from "../../Preloader/Preloader";
 
 const Team = () => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2900);
+  }, []);
   return (
-    <div className="team_main">
-      <TeamHeading />
-      <OurTeam />
-      <ul className="net_lines justify-content-between">
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-      </ul>
-    </div>
+    <>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <motion.div
+          className="team_main"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0, duration: 1 }}
+        >
+          <TeamHeading />
+          <OurTeam />
+          <ul className="net_lines justify-content-between">
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+          </ul>
+        </motion.div>
+      )}
+    </>
   );
 };
 
