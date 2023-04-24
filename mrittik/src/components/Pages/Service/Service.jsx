109b75b1ee79567ffa278cdbb0d64ef327a1c816
Azer Mammadov <mammadovazer98@gmail.com>
2023-04-24 22:24:37 +0400

    Preloader added to Service page

--- mrittik/src/components/Pages/Service/Service.jsx
+++ mrittik/src/components/Pages/Service/Service.jsx
@@ -1,27 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../Service/Service.scss";
 import ServiceHeading from "./ServiceHeading/ServiceHeading";
 import ServiceCards from "./ServiceCards/ServiceCards";
 import BlackSwipeBox from "./BlackSwipeBox/BlackSwipeBox";
 import Ourpartners from "./Ourpartners/Ourpartners";
+import { motion } from "framer-motion";
+import Preloader from "../../Preloader/Preloader";
+
 const Service = () => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2900);
+  }, []);
   return (
-    <div className="service_main">
-      <ServiceHeading />
-      <ServiceCards />
-      <BlackSwipeBox />
-      <Ourpartners />
-      <div className="servicepage_backimage"></div>
-      <ul className="net_lines justify-content-between">
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-      </ul>
-    </div>
+    <>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <motion.div
+          className="service_main"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0, duration: 1 }}
+        >
+          <ServiceHeading />
+          <ServiceCards />
+          <BlackSwipeBox />
+          <Ourpartners />
+          <div className="servicepage_backimage"></div>
+          <ul className="net_lines justify-content-between">
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+          </ul>
+        </motion.div>
+      )}
+    </>
   );
 };
 
