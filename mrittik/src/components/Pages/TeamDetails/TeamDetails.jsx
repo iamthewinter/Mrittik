2dc547a976c7481c9f9328fa92df710811afe329
Azer Mammadov <mammadovazer98@gmail.com>
2023-04-24 22:06:41 +0400

    Preloader was added to Teamdetails page

--- mrittik/src/components/Pages/TeamDetails/TeamDetails.jsx
+++ mrittik/src/components/Pages/TeamDetails/TeamDetails.jsx
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TeamDetailHead from "./TeamDetailHead/TeamDetailHead";
 import "../TeamDetails/TeamDetails.scss";
 import MemberBio from "./MemberBio/MemberBio";
+import { motion } from "framer-motion";
+import Preloader from "../../Preloader/Preloader";
+
 const TeamDetails = () => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2900);
+  }, []);
   return (
-    <div className="teamdetails_main">
-      <TeamDetailHead />
-      <MemberBio />
-      <ul className="net_lines justify-content-between">
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-        <li className="animated_line"></li>
-      </ul>
-    </div>
+    <>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <motion.div
+          className="teamdetails_main"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0, duration: 1 }}
+        >
+          <TeamDetailHead />
+          <MemberBio />
+          <ul className="net_lines justify-content-between">
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+            <li className="animated_line"></li>
+          </ul>
+        </motion.div>
+      )}
+    </>
   );
 };
 
