7d27d5d5b7bb5c0003baa8bf9fecdb639563ada6
Azer Mammadov <mammadovazer98@gmail.com>
2023-04-25 17:43:50 +0400

    Error page was created

--- mrittik/src/components/Pages/Error/Error.jsx
+++ mrittik/src/components/Pages/Error/Error.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import Preloader from "../../Preloader/Preloader";
+import ErrorContent from "./ErrorContent/ErrorContent";
 
 const Error = () => {
-  return <div>Error</div>;
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <motion.div
+          className="error_main"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0, duration: 1 }}
+        >
+          <ErrorContent />
+        </motion.div>
+      )}
+    </>
+  );
 };
 
 export default Error;
---
+++ mrittik/src/components/Pages/Error/ErrorContent/ErrorContent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ErrorContent = () => {
+  return <section className="errorcontent_section"></section>;
+};
+
+export default ErrorContent;
